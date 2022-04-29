41d1ae064ff7cfc4b40a76aa5b019e3f9b0c5d55
Harry Lopez <harry.lopez@overactive.com>
2022-04-28 20:24:30 -0500

    Initial Commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Harry Lopez on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
